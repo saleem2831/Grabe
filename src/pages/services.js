ed147e1d216e06a80afb1631e920abc7e541d166
saleem2831 <shaiksaleem2831@gmail.com>
2023-10-11 13:36:56 +0530

    Work and Services Designed

--- src/pages/services.js
+++ src/pages/services.js
@@ -9,25 +9,67 @@ const Services = () => {
 
       return (
         <main>
-          <section className='intro'>
-            <h1>This is the Introduction section</h1>
-          </section>
+          <div className='Box1'>
+            <div className='Box1-1'>
+              <p className='Box1-1-1'>A full-service digital <br/> innovation partner</p>
+              <p className='Box1-1-2'>Our rich technology and design expertise delivers top brands and <br/> digital experiences.</p>
+            </div>
+
+          </div>
     
-          <section className='contents'>
-            <h1>I Love React</h1>
-          </section>
+          <div className='Tag4'>
+              <div className='Tag4-11'>
+                  {/* <div className='Tag4-1-1'>
+                  </div> */}
+                  <p className='Tag4-1-2'>Digital & Business Transformation</p>
+              <p className='Tag4-1-3'>
+              At GrubStake, we believe everyone deserves an exceptional product experience, whether using a product at work or in their free time. We create memorable enterprise and consumer products as well as provide comprehensive design systems for effortless product iteration.
+              </p>
+              <div className='Tag4-1-4'>
+                  <button className='Btn-4'>Data & Analytics</button>
+                  <button className='Btn-4'>Modernizing Core Technology</button>
+                  <button className='Btn-4'>Modernizing Core Technology</button>
+                  <button className='Btn-4'>Enterprise Technology</button>
+                  <button className='Btn-4'>Automation</button>
+                  <button className='Btn-4'>Change Management</button>
+                  <button className='Btn-4'>Restructuring</button>
+              </div>
+              </div>
+              
+              <div className='Tag4-13'>
+              <div className='Tag4-2-1'>
+              </div>
+            
+              </div>
+            </div>
     
-            <section id="stick">
-              <h1>
-                Hey I'm Sticky
-              </h1>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-            </section>
+           
+              
+
+            <div className='Tag'>
+           <div className='Tag4-13'>
+              <div className='Tag1-2-1'>
+              </div>
+              </div>
+
+              <div className='Tag4-11'>
+                  
+                  <p className='Tag4-1-2'>Growth by design</p>
+              <p className='Tag4-1-3'>
+              Our designers and developers collaborate to create products and digital workflows that provide exceptional user experiences and functionality. We optimise search engine visibility, ensure accessibility, and maximise performance.
+              </p>
+              <div className='Tag4-1-4'>
+                  <button className='Btn-4'>Customer Experience</button>
+                  <button className='Btn-4'>Customer Episode/Journey Design</button>
+                  <button className='Btn-4'>Product Design</button>
+                  <button className='Btn-4'>Service Design</button>
+                  <button className='Btn-4'>Create NPS (Net Promoter System)</button>
+                  
+              </div>
+              </div>
+              
+              
+            </div>
     
           <section className='footer'>
             <h1>Let's end the application with this Footer</h1>
--- src/pages/work.js
+++ src/pages/work.js
@@ -2,37 +2,93 @@
 import React from 'react';
 
 import './work.css';
+import './home.css';
 
 
 const Work = () => {
     return (
         <main>
-          <section className='intro'>
-            <h1>This is the Introduction section</h1>
-          </section>
+          <div className='Intro'>
+            <div className='first'>
+               <p className='first-1'>
+               We bring your boldest ideas to life
+                </p>   
+            </div>
+            </div>
     
-          <section className='contents'>
-            <h1>I Love React</h1>
-          </section>
+          <div className='Tag4'>
+              <div className='Tag4-11'>
+                  <div className='Tag4-1-1'>
+                  </div>
+                  <p className='Tag4-1-2'>Case Study
+              </p>
+              <p className='Tag4-1-3'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              </p>
+              {/* <div className='Tag4-1-4'>
+                  <button className='Btn-4'>Strategy</button>
+                  <button className='Btn-4'>BusinessConsulting</button>
+                  <button className='Btn-4'>Experience</button>
+                  <button className='Btn-4'>Health Care</button>
+                  <button className='Btn-4'>Cloud</button>
+              </div> */}
+              </div>
+              
+              <div className='Tag4-12'>
+              <div className='Tag4-2-1'>
+              </div>
+              <p className='Tag4-2-2'>Case Study
+              </p>
+              <p className='Tag4-2-3'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              </p>
+              {/* <div className='Tag4-2-4'>
+                  <button className='Btn-4'>B2B</button>
+                  <button className='Btn-4'>BusinessConsulting</button>
+                  <button className='Btn-4'>Saas</button>
+              </div> */}
+              </div>
+            </div>
     
-            <section id="stick">
-              <h1>
-                Hey I'm Sticky
-              </h1>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-              <p>other contents</p>
-            </section>
-    
-          <section className='footer'>
-            <h1>Let's end the application with this Footer</h1>
-          </section>
-          <section class="callout">
-  <img src="https://www.osprey.com/images/heros/19_Backpacking_Carousel.jpg" alt="callout"/>
-</section>
+          <div className='Tag5'>
+          <div className='Tag5-1'>
+              </div>
+              <p className='Tag5-2'>Case Study
+              </p>
+              <p className='Tag5-3'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              </p>
+              {/* <div className='Tag4-1-4'>
+                  <button className='Btn-4'>Strategy</button>
+                  <button className='Btn-4'>BusinessConsulting</button>
+                  <button className='Btn-4'>Experience</button>
+                  <button className='Btn-4'>Health Care</button>
+                  <button className='Btn-4'>Cloud</button>
+              </div>          */}
+               </div>
+
+               <div className='Tag6'>
+              <div className='Tag6-11'>
+                  <div className='Tag6-1-1'>
+                  </div>
+                  <p className='Tag6-1-2'>Case Study
+              </p>
+              <p className='Tag6-1-3'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              </p>
+              </div>
+              
+              <div className='Tag6-12'>
+              <div className='Tag6-2-1'>
+              </div>
+              <p className='Tag6-2-2'>Case Study
+              </p>
+              <p className='Tag6-2-3'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              </p>
+            
+              </div>
+            </div>
        
         </main>
       );};
